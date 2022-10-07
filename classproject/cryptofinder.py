80f72a51e3b20233e7028a9dabbf2a76b3dca89c
dbonacuse <dbonacuse@gmail.com>
2022-10-07 07:08:02 +0000

    new cryptofinder application beginnings

---
+++ classproject/cryptofinder.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+
+local_currency = 'USD'
+local_symbol = '$'
+
+headers = {
+        'X-CMC_PRO_API_KEY' : 'a4bfc9df-0ed7-4370-83a9-cc73eeb08dfc' ,
+        'Accepts' : 'application/json'
+        }
+
+base_url = 'https://pro-api.coinmarketcap.com'
+
+global_url = base_url + '/v1/global-metrics/quotes/latest?convert=' + local_currency
+
+# calling the requests library and getting data from API, passing in global url
+# headers = headers dictionary
+request = requests.get(global_url, headers=headers)
+
+# cmc has sent us global metrics, now sorting data w json
+results = request.json()
+
+print(json.dumps(results, sort_keys = True, indent = 4))
+
+data = results["data"]
+
+btc_dominance = data["btc_dominance"]
+eth_dominance = data["eth_dominance"]
+total_market_cap = data["quote"][local_currency]["total_market_cap"]
+total_volume_24h = data["quote"][local_currency]["total_volume_24h"]
+
+btc_dominance = str(round(btc_dominance, 2))+ '%'
+
+eth_dominance = str(round(eth_dominance, 2))+ '%'
+
+tmc_rounded = (round(total_market_cap, 2))
+total_market_cap = '$' + f'{tmc_rounded:,}'
+
+total_volume_24h = '$' + f'{total_volume_24h:,}'
+
+print(btc_dominance)
+print(eth_dominance)
+print(total_market_cap)
+print(total_volume_24h)
+
+
+
+# answer = str(round(answer, 2))
+
+
+
+# while True:
+
+#     # base URLs for application
+#     globalURL = "https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest"
+
+#     tickerURL = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest"
+
+#     # get data from globalURL
+#     request = requests.get(globalURL)
+#     results = request.json()
+#     globalMarketCap = results['data']['quotes']['USD']['total_market_cap']
+
+#     #menu
+#     print()
+#     print("Welcome")
+#     print("Global cap of all cryptocurrencies: $" + str(globalMarketCap))
+#     print("Enter 'all' or 'name of crypto' (i.e. bitcoin) to see the name of the top 100 currencies")
+#     print()
+#     choice = input("Your choice: ")
+
+#     if choice == "all":
+#         request = requests.get(tickerURL)
+#         results = request.json()
+
+#         for x in data:
+#             ticker = x['symbol']
+#             price = x['quote']['USD']['price']
+
+#             print(ticker + ":\t\t$" + price)
+#         print()
+
+#     else:
+#         tickerURL += '/' + choice + '/'
+#         request = requests.get(globalURL)
+#         results = requests.json()
+
+#         ticker = data[0]['symbol']
+#         price = data[0]['quote']['USD']['price']
+
+#         print(ticker + ":\t\t$" + price)
+#         print()
+    
+#     choice2 = input("Again? (y/n)")
+#     if choice2 == "y":
+#         continue
+#     if choice2 == "n":
+#         break
+        
+
--- pythonproject/cryptocurrency.py
+++ pythonproject/cryptocurrency.py
@@ -3,43 +3,51 @@
         Cryptocurrency data application """
 
 import requests
-from requests import Request, Session
+# from requests import Request, Session
 import pprint
 import json
 
-#  https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest
+#  https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest
+
+# API url
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 
+# Dictionary of GET parameters to send with request
 params = {
        'start' : '1',
-       'limit' : '10',
+     #  'limit' : '10',
        'convert' : 'USD'
         }
 
+# Dictionary of HTTP headers to send with request
 headers = {
         'X-CMC_PRO_API_KEY' : 'a4bfc9df-0ed7-4370-83a9-cc73eeb08dfc' ,
         'Accepts' : 'application/json'
         }
 
+# json data that is being returned
 json = requests.get(url, params=params, headers=headers).json()
 
+# parsing 'data' object
 coins = json['data']
 
-""" def symbol_finder(coins):
-        symbols = []
-        for x in coins:
-                req = requests.get(x)
-                decodedjson = req.json()
-                symbols.append(decodedjson.get("symbol"))
-
-        return symbols """
-
+# main function
 def main():
 
-        search_input = input("Please search for a cryptocurrency: ")
+        # Asking user to input a symbol for a listed cryptocurrency
+       # search_input = input("Please search for a cryptocurrency: ")
 
-        for search_input in coins :
-                 print(search_input['cmc_rank'], search_input['symbol'], search_input['quote']['USD']['price'])
+
+        for x in coins :
+        # if x['symbol'] == 'BTC' :
+                print (x['cmc_rank'], x['symbol'], x['quote']['USD']['price'])
+
+        # else:
+        #         print("Please enter a valid symbol")
+
+
+        # for search_input in coins :
+        #          print(search_input['cmc_rank'],search_input['symbol'],search_input['quote']['USD']['price'])
 
 main()
 # session = Session()
